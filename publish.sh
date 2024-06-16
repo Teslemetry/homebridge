85fc5201a558aaffdf84aa016856efdb10978b92
Brett <brett.whynot@gmail.com>
2024-06-16 21:15:02 +1000

    update publisher

--- publish.sh
+++ publish.sh
@@ -1,9 +1,11 @@
 git checkout teslemetry
+npm version patch
 git rebase dev
 git push --force-with-lease
 npm run publish
 
 git checkout tessie
+npm version patch
 git rebase dev
 git push --force-with-lease
 npm run publish
