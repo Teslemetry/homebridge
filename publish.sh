250e199f0ef90db85d161382e4b9ea14d91426e3
Brett <brett.whynot@gmail.com>
2024-06-18 08:57:59 +1000

    deployment fixes

--- publish.sh
+++ publish.sh
@@ -6,14 +6,18 @@ npm version minor
 git push
 
 git checkout teslemetry
-git rebase dev -X ours
+git rebase dev
 git push --force-with-lease
+
+git checkout tessie
+git rebase dev
+git push --force-with-lease
+
+git checkout teslemetry
 rm -r dist
 npm publish
 
 git checkout tessie
-git rebase dev  -X ours
-git push --force-with-lease
 rm -r dist
 npm publish
 
