12cea9ee2be3ac87da6cca8715b5875c055b30d3
Brett <brett.whynot@gmail.com>
2024-06-17 20:36:12 +1000

    fix rebases

--- publish.sh
+++ publish.sh
@@ -1,13 +1,15 @@
+npm run prepublishOnly
+npm version patch
 git push
 
 git checkout teslemetry
 git rebase dev
-npm version patch
 git push --force-with-lease
 npm run publish
 
 git checkout tessie
 git rebase dev
-npm version patch
 git push --force-with-lease
 npm run publish
+
+git checkout dev
