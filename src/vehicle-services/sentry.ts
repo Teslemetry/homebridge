e60b78e63640bbe185ed3d10d47e33ae6abe16d4
Brett <brett.whynot@gmail.com>
2024-06-21 20:38:35 +1000

    Make sentry a security system

--- src/vehicle-services/sentry.ts
+++ src/vehicle-services/sentry.ts
@@ -3,18 +3,25 @@ import { BaseService } from "./base.js";
 
 export class SentryService extends BaseService {
   constructor(parent: VehicleAccessory) {
-    super(parent, parent.platform.Service.Switch, "Sentry", "sentry");
+    super(parent, parent.platform.Service.SecuritySystem, "Sentry", "sentry");
 
-    const on = this.service
-      .getCharacteristic(this.parent.platform.Characteristic.On)
+    const current = this.service
+      .getCharacteristic(this.parent.platform.Characteristic.SecuritySystemCurrentState);
+
+    const target = this.service
+      .getCharacteristic(this.parent.platform.Characteristic.SecuritySystemTargetState)
       .onSet(async (value) => {
+        value = value === 3 ? 3 : 0;
+        target.updateValue(value);
         await this.parent.wakeUpAndWait()
-          .then(() => this.vehicle.set_sentry_mode(value as boolean))
-          .then(() => on.updateValue(value));
+          .then(() => this.vehicle.set_sentry_mode(value !== 3))
+          .then(() => current.updateValue(value));
       });
 
     this.parent.emitter.on("vehicle_data", (data) => {
-      on.updateValue(data.vehicle_state.sentry_mode);
+      const value = data.vehicle_state.sentry_mode ? 0 : 3;
+      current.updateValue(value);
+      target.updateValue(value);
     });
   }
 }
