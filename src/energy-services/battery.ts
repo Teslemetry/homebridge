f39ca67bbabd1aa5787283506205412908044da1
Brett <brett.whynot@gmail.com>
2025-06-03 20:08:03 +1000

    lint

--- src/energy-services/battery.ts
+++ src/energy-services/battery.ts
@@ -5,23 +5,32 @@ export class BatteryService extends BaseService {
   constructor(parent: EnergyAccessory) {
     super(parent, parent.platform.Service.Battery, "Battery", "battery");
 
-    const batteryLevel = this.service
-      .getCharacteristic(this.parent.platform.Characteristic.BatteryLevel);
+    const batteryLevel = this.service.getCharacteristic(
+      this.parent.platform.Characteristic.BatteryLevel,
+    );
 
-    const chargingState = this.service
-      .getCharacteristic(this.parent.platform.Characteristic.ChargingState);
+    const chargingState = this.service.getCharacteristic(
+      this.parent.platform.Characteristic.ChargingState,
+    );
 
-    const lowBattery = this.service
-      .getCharacteristic(this.parent.platform.Characteristic.StatusLowBattery);
+    const lowBattery = this.service.getCharacteristic(
+      this.parent.platform.Characteristic.StatusLowBattery,
+    );
 
     this.parent.emitter.on("live_status", (data) => {
       batteryLevel.updateValue(data.percentage_charged ?? 50);
-      chargingState.updateValue((data.battery_power ?? 0) < 0 
-        ? this.parent.platform.hap.Characteristic.ChargingState.CHARGING 
-        : this.parent.platform.hap.Characteristic.ChargingState.NOT_CHARGING);
-      lowBattery.updateValue((data.percentage_charged ?? 50) <= 20 
-        ? this.parent.platform.hap.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW 
-        : this.parent.platform.hap.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
+      chargingState.updateValue(
+        (data.battery_power ?? 0) < 0
+          ? this.parent.platform.hap.Characteristic.ChargingState.CHARGING
+          : this.parent.platform.hap.Characteristic.ChargingState.NOT_CHARGING,
+      );
+      lowBattery.updateValue(
+        (data.percentage_charged ?? 50) <= 20
+          ? this.parent.platform.hap.Characteristic.StatusLowBattery
+              .BATTERY_LEVEL_LOW
+          : this.parent.platform.hap.Characteristic.StatusLowBattery
+              .BATTERY_LEVEL_NORMAL,
+      );
     });
   }
 }
